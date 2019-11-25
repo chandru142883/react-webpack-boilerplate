916b36b2aef87f5a637cf5eeff674cc5f5b1b4b2
Chandrasheakran Marisamy <Chandrasheakran.Marisamy@verisk.com>
2019-11-24 23:57:46 -0500

    commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// Seting up React with web pack
+//Reference Site: https://www.freecodecamp.org/news/part-1-react-app-from-scratch-using-webpack-4-562b1d231e75/
+
+import React from "react";
+import ReactDOM from "react-dom";
+const App = () => {
+    return <div>Hello React!</div>;
+  };
+ReactDOM.render(<App />, document.getElementById("index")); 
